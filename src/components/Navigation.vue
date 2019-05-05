16f5fe35a7d6a04e64402517f5cda8d0d8a4b0f6
alex <alex@localhost>
2019-05-05 22:04:01 +0300

    !!!

---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+    <span>
+        <v-navigation-drawer app v-model="drawer" disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index">
+                        <v-list-tile-content>
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app>
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+        </v-toolbar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            drawer: false,
+            items: [
+                { title: 'Menu' },
+                { title: 'Sign In' },
+                { title: 'Join' }
+            ]
+        };
+    }
+};
+</script>
+
+<style>
+    .theme--light
+        .v-toolbar {
+            background-color: transparent;
+            box-shadow: none;
+        }
+</style>
